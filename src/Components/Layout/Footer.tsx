ecd255e36d398dc8bac0ee34b1e94b672dd171d0
Kronos <dimamitru@gmail.com>
2023-12-27 12:17:36 +0200

    Initial commit

---
+++ src/Components/Layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer fixed-bottom text-center p-3 bg-dark text-white">
+      &copy; Made with <i className="bi bi-emoji-tear-fill"></i> by Master Shifu
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/Container/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Footer, Header } from "../Components/Layout";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      Main Component
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
